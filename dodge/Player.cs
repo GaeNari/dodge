2affa3d4eb123caaca90dcf6260b362087032f8b
GaeNari <gaenari31@gmail.com>
2013-05-01 21:11:15 +0900

    make player and move

---
+++ dodge/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace dodge
+{
+    class Player
+    {
+        public int X = 300;
+        public int Y = 300;
+
+        public void moveLeft()
+        {
+            this.X -= 1;
+        }
+        public void moveRight()
+        {
+            this.X += 1;
+        }
+        public void moveUp()
+        {
+            this.Y -= 1;
+        }
+        public void moveDown()
+        {
+            this.Y += 1;
+        }
+
+        public void Draw(Graphics g)
+        {
+            Image im = Properties.Resources.munchul;
+            g.DrawImage(im, new Point(X - im.Width / 2, Y - im.Height / 2));
+        }
+    }
+}
