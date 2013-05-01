80bd842db3b0de2fd45cf70d9c5a6a5351b1ba0b
GaeNari <gaenari31@gmail.com>
2013-05-01 21:13:28 +0900

    make projectile

---
+++ dodge/OdukKing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dodge
+{
+    class OdukKing
+    {
+        public int X;
+        public int Y;
+
+    }
+}
