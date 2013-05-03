dd6413a58335dabfd10183b7fc251a5b35e0ab08
GaeNari <gaenari31@gmail.com>
2013-05-04 04:38:07 +0900

    make odukking class

--- dodge/OdukKing.cs
+++ dodge/OdukKing.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace dodge
 {
@@ -10,5 +11,29 @@ namespace dodge
         public int X;
         public int Y;
 
+        public int X_speed;
+        public int Y_speed;
+
+        public static List<OdukKing> projectiles = new List<OdukKing>();
+
+        public OdukKing(int X, int Y, int X_speed, int Y_speed)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.X_speed = X_speed;
+            this.Y_speed = Y_speed;
+        }
+
+        public void move()
+        {
+            X += X_speed;
+            Y += Y_speed;
+        }
+
+        public void Draw(Graphics g)
+        {
+            Image im = Properties.Resources.odukking;
+            g.DrawImage(im,new Point(X - im.Width / 2, Y - im.Height / 2));
+        }
     }
 }
