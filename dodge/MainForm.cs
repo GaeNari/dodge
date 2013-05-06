9c094fd747b570598100a96a6db8ffa8b138ed48
GaeNari <gaenari31@gmail.com>
2013-05-06 17:33:38 +0900

    make code more readable

--- dodge/MainForm.cs
+++ dodge/MainForm.cs
@@ -67,34 +67,28 @@ namespace dodge
                             if (new_X == player.X) new_X--;
                             new_Y = 5;
                             if (new_Y == player.Y) new_Y--;
-                            new_X_Speed = 200 * ((player.X - new_X) / Math.Abs(player.X - new_X));
-                            new_Y_Speed = 200 * ((player.Y - new_Y) / Math.Abs(player.Y - new_Y));
                             break;
                         case 1:
                             new_X = r.Next(5, 980);
                             if (new_X == player.X) new_X--;
                             new_Y = 720;
                             if (new_Y == player.Y) new_Y--;
-                            new_X_Speed = 200 * ((player.X - new_X) / Math.Abs(player.X - new_X));
-                            new_Y_Speed = 200 * ((player.Y - new_Y) / Math.Abs(player.Y - new_Y));
                             break;
                         case 2:
                             new_X = 5;
                             if (new_X == player.X) new_X--;
                             new_Y = r.Next(5, 720);
                             if (new_Y == player.Y) new_Y--;
-                            new_X_Speed = 200 * ((player.X - new_X) / Math.Abs(player.X - new_X));
-                            new_Y_Speed = 200 * ((player.Y - new_Y) / Math.Abs(player.Y - new_Y));
                             break;
                         default:
                             new_X = 980;
                             if (new_X == player.X) new_X--;
                             new_Y = r.Next(5, 720);
                             if (new_Y == player.Y) new_Y--;
-                            new_X_Speed = 200 * ((player.X - new_X) / Math.Abs(player.X - new_X));
-                            new_Y_Speed = 200 * ((player.Y - new_Y) / Math.Abs(player.Y - new_Y));
                             break;
                     }
+                    new_X_Speed = 200 * ((player.X - new_X) / Math.Abs(player.X - new_X));
+                    new_Y_Speed = 200 * ((player.Y - new_Y) / Math.Abs(player.Y - new_Y));
                     odukKings[odukCount] = new OdukKing(new_X, new_Y, new_X_Speed, new_Y_Speed);
                     odukCount++;
                 }
